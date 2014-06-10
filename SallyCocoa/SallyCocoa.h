dc762580865d8eb1d8c5a0b61148ffa258af5206
Zaid Al-Jarrah <zaid.aljarrah@gmail.com>
2014-06-09 21:42:12 -0400

    Initial Commit

---
+++ SallyCocoa/SallyCocoa.h
@@ -0,0 +1,13 @@
+//
+//  SallyCocoa.h
+//  SallyCocoa
+//
+//  Created by Zaid Al-Jarrah on 2014-06-09.
+//  Copyright (c) 2014 Tokenize. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SallyCocoa : NSObject
+
+@end
---
+++ SallyCocoa/SallyCocoa.m
@@ -0,0 +1,13 @@
+//
+//  SallyCocoa.m
+//  SallyCocoa
+//
+//  Created by Zaid Al-Jarrah on 2014-06-09.
+//  Copyright (c) 2014 Tokenize. All rights reserved.
+//
+
+#import "SallyCocoa.h"
+
+@implementation SallyCocoa
+
+@end
